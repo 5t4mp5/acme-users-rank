f41b8c291ff7826e91334756d5873448512eec14
Dave <dave.e.siegel@gmail.com>
2019-04-01 16:54:01 -0400

    fix to last commit

--- src/components/Main.js
+++ src/components/Main.js
@@ -56,12 +56,10 @@ class Main extends Component {
         {this.props.errors.length > 0 ? (
           <ul className="alert alert-danger">
             {this.props.errors.map((error, i) => {
-              return error.errors ? (
+              return error.errors && (
                 error.errors.map((_error, j) => {
                   return <li key={i + j + _error.message}>{_error.message}</li>;
                 })
-              ) : (
-                ""
               );
             })}
           </ul>
